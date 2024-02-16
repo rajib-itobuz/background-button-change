58a4d95a5fc5898298be2f26e130dc27724f5490
rajib-itobuz <rajib@itobuz.com>
2024-02-16 17:24:51 +0530

    changes: change layout of proj

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const colors = [
   "linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%)",
   "linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%)",
   "linear-gradient(90deg, #FDBB2D 0%, #22C1C3 100%)",
-  "linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%)",
   "linear-gradient(to right, #0f2027, #203a43, #2c5364)",
+  "linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%)",
   "linear-gradient(to right, #0f2027, #203a43, #2c5364)",
 ];
 
@@ -14,16 +14,6 @@ const recalculate = () => {};
 const callChangeBackground = (e) => {
   console.log(buttonsClasses);
   document.body.style.background = colors[e];
-  buttonsClasses[e].setAttribute("disabled", "true");
-
-  if (e < 5) {
-    const newButton = document.createElement("button");
-    newButton.innerText = `Color ${e + 2}`;
-    newButton.setAttribute("onclick", `callChangeBackground(${e + 1})`);
-
-    document.body.append(newButton);
-    buttonsClasses = document.querySelectorAll("button");
-  }
 };
 
 buttonsClasses.forEach((e, ind) =>
